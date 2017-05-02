a5034bb86d27395238a7034f86a4009da52c3697
alyssadmoore <alyssadmoore@gmail.com>
2017-05-02 12:10:09 -0500

    Add rudimentary search

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var hbs = require('express-handlebars');
 var session = require('express-session');
-var MongoDBStore = require('connect-mongodb-session')(session);
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var expressValidator = require('express-validator');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -20,53 +22,47 @@ app.engine('.hbs', hbs({
     partialsDir: path.join(__dirname, 'views/partials')
 }));
 
+// TODO setup sessions
+
 var mongo_pw = process.env.MONGO_PW;
-var url = 'mongodb://mongo:' + mongo_pw + '@localhost:27017/pokemon?authSource=admin';
+var url = 'mongodb://mongo:' + mongo_pw + '@localhost:27017/pokesite?authSource=admin';
+MongoClient.connect(url, function(err, db){
+    assert.equal(null, err);
+    console.log('Connected to MongoDB');
 
-var store = new MongoDBStore({
-    uri: url,
-    collection: 'sessions'
-}, function(err){
-    console.log(err)
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'hbs');
+
+    // uncomment after placing your favicon in /public
+    //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(expressValidator());
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use('/', index);
+    app.use('/users', users);
+
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      var err = new Error('Not Found');
+      err.status = 404;
+      next(err);
+    });
+
+    // error handler
+    app.use(function(err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
 });
 
-app.use(session({
-    secret: '3k3lkm6576lkmew098fknmf9876g3n',
-    resave: false,
-    saveUninitialized: false,
-    store: store
-}));
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,20 @@ var P = new Pokedex();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    P.getPokemonByName('eevee') // with Promise
-        .then(function(response) {
-            console.log(response);
-        })
-        .catch(function(error) {
-            console.log('There was an ERROR: ', error);
-        });
   res.render('index');
 });
+
+// TODO send back name & sprite as well
+router.get('/searchPokemon', function(req, res, next){
+    P.getPokemonByName(req.query.pokemon_name).then(function(response){
+        res.render('index', {pokemon: response['id']})
+    }).catch(function(err){
+        console.log('Error:', err)
+    });
+});
+
+// router.get('/savePokemon', function(req, res, next){
+//
+// });
 
 module.exports = router;
